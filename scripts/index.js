6f07ef7dc19007cb4f782f9682c0949750c20b6a
CarlosPalmaSantos <77320870+MagincianCompany@users.noreply.github.com>
2023-05-24 14:22:58 +0200

    first

---
+++ scripts/index.js
@@ -0,0 +1,63 @@
+function loadForm()
+{ 
+    var descripcionSection = document.getElementById('descripcion');
+  descripcionSection.style.display = 'none';
+  
+  // Mostrar el iframe
+  var iframe = document.getElementById('content-frame');
+  iframe.style.display = 'block';
+  
+  var title = document.getElementById('titulo');
+  title.style.display="none";
+  
+
+  var header = document.getElementById('header');
+  header.style.margin="0px";
+  header.style.padding="0px";
+  header.style.width="100%";
+
+    header = document.getElementById('ul');
+  header.style.margin="0px";
+  header.style.padding="0px";
+  header.style.paddingBottom="0.6em";
+
+
+  var lis = document.getElementsByClassName('li');
+  for(let i = 0; i<lis.length;i++)
+  {
+        lis[i].style.fontSize='0.75em';
+        lis[i].style.padding='1em';
+  }
+
+  var brid = document.getElementsByClassName('brid');
+  for(let i = 0; i<brid.length;i++)
+  {
+    var brElement = brid[i];
+    var space = document.createTextNode(' ');
+
+    brElement.parentNode.replaceChild(space, brElement);
+  }
+
+  
+  var liElements = document.querySelectorAll('li'); // Selecciona todos los elementos <li>
+
+  liElements.forEach(function(liElement) {
+    var liAfterElement = window.getComputedStyle(liElement, '::after'); // Obtiene las propiedades calculadas del pseudo-elemento
+
+    // Modifica el estilo del pseudo-elemento ::after
+    liElement.style.setProperty('--li-after-width', "2px");
+    liElement.style.setProperty('--li-after-height', "30px");
+  });
+
+
+}
+
+function hideForm()
+{
+    var iframe = document.getElementById('content-frame');
+    iframe.style.display = 'none';
+
+    var descripcionSection = document.getElementById('descripcion');
+    descripcionSection.style.display = 'block';
+
+}
